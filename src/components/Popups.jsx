c53b8aadca3906298d7f0d7f1b0f74f1b9151c93
Michael Cote <michaelcote7@outlook.com>
2020-08-12 19:24:26 -0700

    .

--- src/components/Popups.jsx
+++ src/components/Popups.jsx
@@ -23,7 +23,7 @@ const Popups = () => {
     $("#poptwo").mouseleave(function(){
         $("#poptwo").removeClass(" appear")
       })
-    //-----------------------------------------------
+    //----------------------------------------------
     $("#hoverthree").mouseenter(function(){
         $("#popthree").addClass(" appear")
       })
@@ -33,7 +33,7 @@ const Popups = () => {
     $("#popthree").mouseleave(function(){
         $("#popthree").removeClass(" appear")
       })
-    //-----------------------------------------------
+    //----------------------------------------------
     $("#hoverfour").mouseenter(function(){
         $("#popfour").addClass(" appear")
       })
@@ -43,15 +43,27 @@ const Popups = () => {
     $("#popfour").mouseleave(function(){
         $("#popfour").removeClass(" appear")
       })
+    //----------new--------------------------------
+    $("#shift").mouseenter(function(){
+      $(".popsubtitle").animate({ top: '200px' } , 'slow')
+      $(".popsubtitletop").animate({ top: '495px' }, 1200)
+      })
+    $("#shift").mouseleave(function(){
+      $(".popsubtitle").animate({ top: '450px' } , 'slow')
+      $(".popsubtitletop").animate({  top: '300px' }, 1200)
+      })
+    //--------------------------------------------
   })
   return (
     <div className="popCont">
-      <h1 className="poptitle">
+      <h4 className="popsubtitle subtitle">check this out</h4>
+      <h1 id="shift" className="poptitle">
         <span id="hoverone">hover</span>&nbsp;
         <span id="hovertwo">over&nbsp;</span>
         <span id="hoverthree">these</span>&nbsp;
         <span id="hoverfour">words</span>
       </h1>
+      <h4 className="popsubtitletop subtitle">check this out</h4>
       <div id="popone" className="boxOne">
         <p>hello</p><hr/>
         <p>hello</p><hr/>
